e6825f05a2e149a2197b84f8da9344d0a467ca5f
Teddy <teddymathias@gmail.com>
2023-01-25 11:47:11 -0500

    Fourth

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,12 @@
 import styles from './Footer.module.css'
 
 export default function Footer() {
-    return ( <
-        >
-        <
-        footer className = { styles.footer } >
-        <
-        img src = ""
-        alt = ""
-        className = { styles.logo }
-        />  < /
-        footer > <
-        />
-    )
+  return (
+    <>
+      <footer className={styles.footer}>
+      
+      </footer>
+    </>
+  )
 }
+
--- pages/index.js
+++ pages/index.js
@@ -3,30 +3,17 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
-    return ( <
-        div className = "container" >
-        <
-        Head >
-        <
-        title > Next.js Starter! < /title> <
-        link rel = "icon"
+    return ( <div className = "container" >
+        <Head >
+        <title > Next.js Starter! </title> 
+        <link rel = "icon"
         href = "/favicon.ico" / >
-        <
-        /Head>
-
-        <
-        main >
-        <
-        Header title = "Coming soon!" / >
-        <
-        p className = "description" >
-        In the meantime visit < a href: "www.teddymathias.com" > teddymathias.com < /a> < /
-        p > <
-        /main>
-
-        <
-        Footer / >
-        <
-        /div>
+        </Head>
+        <main >
+        <Header title = "Coming soon!" / >
+        <p className = "description" >
+        In the meantime visit < a href="www.teddymathias.com"> teddymathias.com </a> </p> </main>
+        <Footer/>
+        </div>
     )
 }
