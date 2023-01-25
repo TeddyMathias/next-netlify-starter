d18fe82b507b83e69ce23169a322afd12b8279b5
Teddy <teddymathias@gmail.com>
2023-01-25 11:06:25 -0500

    First push

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,30 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return ( <
+        div className = "container" >
+        <
+        Head >
+        <
+        title > Next.js Starter! < /title> <
+        link rel = "icon"
+        href = "/favicon.ico" / >
+        <
+        /Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+        <
+        main >
+        <
+        Header title = "Welcome to my app!" / >
+        <
+        p className = "description" >
+        Hello my name is Teddy < code > pages / index.js < /code> <
+        /p> <
+        /main>
 
-      <Footer />
-    </div>
-  )
+        <
+        Footer / >
+        <
+        /div>
+    )
 }
